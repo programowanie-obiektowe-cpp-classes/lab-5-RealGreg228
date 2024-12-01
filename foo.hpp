3615e864af1fd977541eff3734436c6faffc471d
Grzegorz Kozera <01171661@pw.edu.pl>
2024-12-01 18:14:17 +0100

    zej5

--- foo.hpp
+++ foo.hpp
@@ -7,6 +7,15 @@
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    // Twoja implementacja tutaj
-    return {};
+    std::vector<char> yes_or_no;
+    for(auto& person : people){
+        person.birthday();
+        if (person.isMonster()) {
+            yes_or_no.push_back('n');
+        } else {
+            yes_or_no.push_back('y');
+        }
+    }
+    std::reverse(yes_or_no.begin(), yes_or_no.end());
+    return yes_or_no;
 }
